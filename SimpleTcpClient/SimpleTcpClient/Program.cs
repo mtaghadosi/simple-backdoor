7b8164776f5b5d870f77f230399b96f1f3add787
Mohammad Taghadosi <mohammad.taghadosi@gmail.com>
2022-10-08 13:20:05 +0330

    Add files via upload

---
+++ SimpleTcpClient/SimpleTcpClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SimpleTcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            int recv;
+            string Phrase;
+            
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+                Console.WriteLine("Successfuly Connected to server...");
+                Thread.Sleep(300);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to Connect to Server!");
+                Console.ReadLine();
+                return;
+            }
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Please enter your request:(type 'e' for Quit)");
+                Phrase = Console.ReadLine().ToLower();
+                server.Send(Encoding.ASCII.GetBytes(Phrase));
+                Console.WriteLine("Data has been sent, waiting for results:");
+                Thread.Sleep(200);
+                recv = server.Receive(data);
+                if (Encoding.ASCII.GetString(data, 0, recv) == "exit" || Encoding.ASCII.GetString(data, 0, recv)=="e")
+                {
+                    break;
+                }
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+                
+            }
+            server.Close();
+        }
+
+        
+    }
+}
---
+++ SimpleTcpServer/SimpleTcpServer/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SimpleTcpServer
+{
+    class Program
+    {
+        public static int recv, Result;
+        static void Main(string[] args)
+        {
+            char[] determine = { ',' };
+            byte[] data = new byte[1024];
+            string Received;
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            Socket newSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            newSock.Bind(ipep);
+            newSock.Listen(10);
+            Console.WriteLine("Waiting for a client...");
+
+            Socket client = newSock.Accept();
+
+            
+            //Must_send = Console.ReadLine();
+
+            //client.Send(Encoding.ASCII.GetBytes(Must_send));
+
+            //Console.WriteLine("\n\nYour data has been sent, Waiting for results...\n\n\n\n");
+
+            //recv = client.Receive(data);
+            //string result_recieved = Encoding.ASCII.GetString(data, 0, recv);
+            //Console.WriteLine(result_recieved);
+
+
+
+            while (true)
+            {
+                Console.WriteLine("\n\nAn Incomming Connection Detected, Waiting for requests...");
+                recv = client.Receive(data);
+                Received = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine("\n\nIncoming Request Detected, Calculating...");
+                if (recv == 0)
+                {
+                    Console.WriteLine("\n\nProblem in Receiving Data ...Exiting!");
+                    break;
+                }
+                
+                Console.WriteLine("\n\nReceived Data is:   " + Received);
+                Thread.Sleep(300);
+                if (Received == "exit" || Received=="e")
+                {
+                    data = Encoding.ASCII.GetBytes("exit");
+                    client.Send(data, SocketFlags.None);
+                    break;
+                }
+                Calculate_Data(determine, Received);
+                data = Encoding.ASCII.GetBytes(Result.ToString());
+                client.Send(data, SocketFlags.None);
+                Console.WriteLine("\n\nData Sent...!");
+            }
+            client.Close();
+            newSock.Close();
+        }
+
+        private static void Calculate_Data(char[] determine, string Received)
+        {
+            string[] Splited = Received.Split(determine);
+
+            if (Splited.Length != 3)
+            {
+                //payam khata be karbar ersal shavad.
+            }
+
+            if (Splited[2] == "+")
+            {
+                Result = int.Parse(Splited[0]) + int.Parse(Splited[1]);
+            }
+            if (Splited[2] == "-")
+            {
+                Result = int.Parse(Splited[0]) - int.Parse(Splited[1]);
+            }
+            if (Splited[2] == "*")
+            {
+                Result = int.Parse(Splited[0]) * int.Parse(Splited[1]);
+            }
+        }
+    }
+}
